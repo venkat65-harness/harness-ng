39b70c8da0b0c76540b0d784ae98485805f42936
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2023-04-07 09:46:25 -0400

    Create main.tf

---
+++ terraform/s3-bucket/main.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "example" {
+  bucket = var.my_bucket
+
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+}
+
