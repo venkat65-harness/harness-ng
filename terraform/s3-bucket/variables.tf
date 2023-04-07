9c3d34f3971f94d9d726e7755c3dd4df543a0e19
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2023-04-07 09:51:05 -0400

    Create variables.tf

---
+++ terraform/s3-bucket/variables.tf
@@ -0,0 +1,25 @@
+# Input variable definitions
+
+variable "minio_region" {
+ default = "us-east-1"
+}
+
+variable "minio_server" {
+ default = "https://play.min.io:9000"
+}
+
+variable "minio_access_key" {
+  default = "VMPNvKWln5wcc0Qk"
+}
+
+variable "minio_secret_key" {
+ default = "iC5ltzuoFnqvibJIpGtMchqQZ5wUPUsL"
+}
+
+variable "my_bucket" {
+ default = "my-bucket-venkat-05"
+}
+
+variable "my_file" {
+ default = "sample.txt"
+}
